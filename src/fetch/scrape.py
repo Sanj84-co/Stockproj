0dee1999c8315f63563264ba57d411fee8cfde2a
Sanj84-co <Sansenus123@gmail.com>
2025-10-23 17:37:31 -0400

    working on cleaning the data. ignore the seaborn errors

--- src/fetch/scrape.py
+++ src/fetch/scrape.py
@@ -1,11 +1,14 @@
 import yfinance as yf 
 import pandas as pa 
+import seaborn as s
 def take(str):
-    data = yf.download(str)
-    clean(data)
-    return (data)
+    dat = yf.download(str)
+    clean(dat)
+    return (dat)
 
-def clean(data):
-    df1 = pa.DataFrame(data)
-    print(pa.isna(df1))
-
+def clean(dat):
+    df1 = pa.DataFrame(dat)
+    pa.isnull(df1)
+    df1.fillna(0)
+    print(df1.duplicated())
+    df1 = df1.drop_duplicates() ##duplicates, null values, 
--- src/main/Back.py
+++ src/main/Back.py
@@ -2,25 +2,30 @@
 from src.fetch.scrape import take
 import matplotlib.pyplot as MatPlot
 import numpy as np
+import pandas as pd 
+import seaborn as ss
 str = input("Please tell which stocks' data you want ")
 dat = take(str)
-def graphit(data):
+def graphit(dat):
     graph = input("What type of data on your ticker do you want for to be plotted:")
     graph2 = input("What type of data on your ticker do you want for to be plotted:")
-    print(data)
-    new = np.array(data[graph])
+    ##print(data)
+    new = np.array(dat[graph])
     np.diff(new)
-    print(new)
-    new2 = np.array(data[graph2])
+    ##print(new)
+    new2 = np.array(dat[graph2])
     np.diff(new2)
     Operation = input("what type of operation do you want to perform on the data")
     a = new2
-    print(a)
+    ##print(a)
+    df = pd.DataFrame(new)
+    ss.catplot(data=df,x='Close',y='Open',hue=None,row=None,col=None,kind="box", jitter= False)
+    MatPlot.show()
     fig,ax = MatPlot.subplots()
     ax.scatter(a[:-1],a[:-1],c = "#1f77b4",marker=".",linewidths= 1.5, edgecolors = 'face', colorizer='none',plotnonfinite = False)
     ax.grid(True)
     fig.tight_layout()
-    MatPlot.show()
+    ##MatPlot.show()
 
 
 graphit(dat)
