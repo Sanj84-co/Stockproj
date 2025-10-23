b297cc2d9c017616064d26d970de0a0ff53f9f6c
Sanj84-co <Sansenus123@gmail.com>
2025-10-23 16:46:41 -0400

    New changes

--- src/fetch/scrape.py
+++ src/fetch/scrape.py
@@ -1,5 +1,11 @@
 import yfinance as yf 
+import pandas as pa 
 def take(str):
     data = yf.download(str)
+    clean(data)
     return (data)
+
+def clean(data):
+    df1 = pa.DataFrame(data)
+    print(pa.isna(df1))
 
