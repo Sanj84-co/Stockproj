829f63c802a0793a9282b20b78be4581d756bdb1
Sanj84-co <Sansenus123@gmail.com>
2025-10-25 00:08:20 -0400

    finalized the seabornplot after reading some documentation

--- src/fetch/scrape.py
+++ src/fetch/scrape.py
@@ -1,6 +1,5 @@
 import yfinance as yf 
 import pandas as pa 
-import seaborn as s
 def take(str):
     dat = yf.download(str)
     clean(dat)
@@ -10,5 +9,4 @@ def clean(dat):
     df1 = pa.DataFrame(dat)
     pa.isnull(df1)
     df1.fillna(0)
-    print(df1.duplicated())
     df1 = df1.drop_duplicates() ##duplicates, null values, 
--- src/main/Back.py
+++ src/main/Back.py
@@ -3,29 +3,33 @@ from src.fetch.scrape import take
 import matplotlib.pyplot as MatPlot
 import numpy as np
 import pandas as pd 
-import seaborn as ss
+import seaborn as sns
 str = input("Please tell which stocks' data you want ")
 dat = take(str)
+opCount = dict{
+    '+': +,
+    '-': -
+}
 def graphit(dat):
     graph = input("What type of data on your ticker do you want for to be plotted:")
     graph2 = input("What type of data on your ticker do you want for to be plotted:")
-    ##print(data)
     new = np.array(dat[graph])
     np.diff(new)
-    ##print(new)
     new2 = np.array(dat[graph2])
     np.diff(new2)
-    Operation = input("what type of operation do you want to perform on the data")
-    a = new2
-    ##print(a)
-    df = pd.DataFrame(new)
-    ss.catplot(data=df,x='Close',y='Open',hue=None,row=None,col=None,kind="box", jitter= False)
+    Operation = input("what type of operation do you want to perform on the data") # want to convert the opeartions string into a value
+    df = pd.DataFrame(dat)
+    #df2 = df.set_index(['Close'])
+    df = df.xs( ,level='Ticker',axis=1) #So df has one row which dalled date and two columns which have levels in each, price which is level 0 and contaisn [close, open, ...etc...] whereas level1 contains ticker and 'appl' so basically, you remove levl one and make the level 0 a column 
+    sns.catplot(data=df,x='Close')
     MatPlot.show()
     fig,ax = MatPlot.subplots()
-    ax.scatter(a[:-1],a[:-1],c = "#1f77b4",marker=".",linewidths= 1.5, edgecolors = 'face', colorizer='none',plotnonfinite = False)
+    df2 = np.array(df[graph])
+    np.diff(df2)
+    ax.scatter(df2[:-1],df2[:-1],c = "#1f77b4",marker=".",linewidths= 1.5, edgecolors = 'face', colorizer='none',plotnonfinite = False)
     ax.grid(True)
     fig.tight_layout()
-    ##MatPlot.show()
+    MatPlot.show()
 
 
 graphit(dat)
